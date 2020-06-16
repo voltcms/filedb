71bac17a21d047b4dc225949b199d23d673894ce
Jannis Rondorf <github@jrondorf.de>
2020-06-16 09:38:12 +0200

    first version

--- src/FileDB.php
+++ src/FileDB.php
@@ -1,10 +1,145 @@
 <?php
 
-namespace PragmaPHP\FiledDB;
+namespace PragmaPHP\FileDB;
+
+use \PragmaPHP\Uid\Uid;
 
 /**
 * Flat file DB based on JSON files
 */
-class FiledDB {
+class FileDB {
+
+    const ATTRIBUTE_ID = '_id';
+    const ATTRIBUTE_CREATED = '_created';
+    const ATTRIBUTE_MODIFIED = '_modified';
+    const FILE_EXT_JSON = '.json';
+
+    private $directory;
+
+    /**
+    * @param    string  Directory
+    */
+    public function __construct(string $directory) {
+        // check if directory is existing
+        if (!is_dir($directory)) {
+            if (!mkdir($directory, 0777, true)) {
+                throw new \Exception("Directory " . $directory . " cannot be created");
+            }
+        }
+        else if (!is_writable($directory)) {
+            throw new \Exception("Directory " . $directory . " is not writeable");
+        }
+        $this->directory = $directory;
+    }
+
+    /**
+    * @param    array   Data
+    */
+    public function create(array $data): string {
+        $time = microtime(true);
+        $created = date(DATE_ATOM, round($time));
+        $id = Uid::generate(round($time * 1000));
+        $data[self::ATTRIBUTE_ID] = $id;
+        $data[self::ATTRIBUTE_CREATED] = $created;
+        $this->writeFile($id, $data);
+        return $id;
+    }
+
+    /**
+    * @param    string  Unique ID
+    * @param    array   Data
+    */
+    public function read(string $id = null, array $search_data = null): array {
+        $result = [];
+        if (!empty($id)) {
+            $files = [$this->directory . DIRECTORY_SEPARATOR . $id . self::FILE_EXT_JSON];
+            return $this->readFiles($files);
+        } else if (!empty($search_data)) {
+            // TODO performance, multi search array, wildcard search
+            $files = $this->readAll();
+            foreach ($files as $file) {
+                foreach ($search_data as $search_key => $search_value) {
+                    foreach ($file as $key => $value) {
+                        if ($key == $search_key && strcasecmp($value, $search_value) === 0) {
+                            $result[] = $file;
+                        }
+                    }
+                }
+            }
+        }
+        return $result;
+    }
+
+    /**
+    * @param    string  Unique ID
+    * @param    array   Data
+    */
+    public function readAll(): array {
+        $files = glob($this->directory . DIRECTORY_SEPARATOR . '*' . self::FILE_EXT_JSON);
+        return $this->readFiles($files);
+    }
+
+    /**
+    * @param    string  Unique ID
+    * @param    array   Data
+    */
+    public function update(string $id, array $data): string {
+        $time = microtime(true);
+        $modified = date(DATE_ATOM, round($time));
+        unset($data['_id']);
+        unset($data['_created']);
+        unset($data['_modified']);
+        $file = $this->directory . DIRECTORY_SEPARATOR . $id . self::FILE_EXT_JSON;
+        if(is_file($file)) {
+            $data = array_merge($this->readFile($file), $data);
+            $data[self::ATTRIBUTE_MODIFIED] = $modified;
+            $this->writeFile($id, $data);
+        }
+        return $id;
+    }
+
+    /**
+    * @param    string  Unique ID
+    */
+    public function delete(string $id) {
+        $files = [$this->directory . DIRECTORY_SEPARATOR . $id . self::FILE_EXT_JSON];
+        $this->deleteFiles($files);
+    }
+
+    /**
+    * @param    string  Unique ID
+    */
+    public function deleteAll() {
+        $files = glob($this->directory . DIRECTORY_SEPARATOR . '*' . self::FILE_EXT_JSON);
+        $this->deleteFiles($files);
+    }
+
+    private function writeFile(string $id, array $data) {
+        $file = $this->directory . DIRECTORY_SEPARATOR . $id . self::FILE_EXT_JSON;
+        ksort($data);
+        file_put_contents($file, json_encode($data));
+    }
+
+    private function readFiles(array $files): array {
+        $result = [];
+        foreach($files as $file) {
+            if(is_file($file)) {
+                $result[] = $this->readFile($file);
+            }
+        }
+        return $result;
+    }
+
+    private function readFile($file): array {
+        return json_decode(file_get_contents($file), true);
+    }
+
+    private function deleteFiles(array $files) {
+        foreach($files as $file) {
+            if(is_file($file)) {
+                unlink($file);
+            }
+        }
+    }
 
 }
--- tests/FileDBTest.php
+++ tests/FileDBTest.php
@@ -6,23 +6,72 @@ use \PragmaPHP\Uid\Uid;
 
 class FileDBTest extends TestCase {
 
-    // date_default_timezone_set('UTC');
-    // $time = microtime(true);
-    // echo round($time);
-    // echo '
-    // ';
-    // echo round($time * 1000);
-    // echo '
-    // ';
-    // // 20-06-14T14:54:20+00:00
-    // //echo round($time*1000);
-    // echo date(DATE_ATOM, 1592146956);
-    // echo '
-    // ';
-    // echo date('c', 1592146956);
-
     public function test() {
-        $this->assertTrue(true);
+        $db = new FileDB('testdata/users');
+        $db->deleteAll();
+        $this->assertEmpty($db->readAll());
+
+        $test_data = [
+            'username' => 'Administrator',
+            'password' => 'test'
+        ];
+        $test_data_update_1 = [
+            'username' => 'Tester'
+        ];
+        $test_data_update_2 = [
+            'lastname' => 'Last Name',
+            'password' => ''
+        ];
+
+        $id = $db->create($test_data);
+        $this->assertNotEmpty($id);
+        $data = $db->read($id);
+        $this->assertNotEmpty($data);
+        $this->assertEquals(count($data), 1);
+        $this->assertEquals($data[0]['_id'], $id);
+        $this->assertNotEmpty($data[0]['_created']);
+        $this->assertEquals($data[0]['username'], 'Administrator');
+        $db->update($id, $test_data_update_1);
+        $data = $db->read($id);
+        $this->assertEquals($data[0]['_id'], $id);
+        $this->assertNotEmpty($data[0]['_created']);
+        $this->assertNotEmpty($data[0]['_modified']);
+        $this->assertEquals($data[0]['username'], 'Tester');
+        $db->update($id, $test_data_update_2);
+        $data = $db->read($id);
+        $this->assertEquals($data[0]['_id'], $id);
+        $this->assertNotEmpty($data[0]['_created']);
+        $this->assertNotEmpty($data[0]['_modified']);
+        $this->assertEquals($data[0]['username'], 'Tester');
+        $this->assertEquals($data[0]['lastname'], 'Last Name');
+        $this->assertEmpty($data[0]['password']);
+
+        $data = $db->read(null, [
+            'username' => 'Tester'
+        ]);
+        $this->assertEquals($data[0]['_id'], $id);
+        $this->assertNotEmpty($data[0]['_created']);
+        $this->assertNotEmpty($data[0]['_modified']);
+        $this->assertEquals($data[0]['username'], 'Tester');
+
+        $data = $db->read(null, [
+            'username' => 'xxx'
+        ]);
+        $this->assertEmpty($data);
+
+        $this->assertNotEmpty($db->readAll());
+        $db->delete($id);
+        $this->assertEmpty($db->read($id));
+
+        $db->create($test_data);
+        $db->create($test_data);
+        $data = $db->readAll();
+        $this->assertNotEmpty($data);
+        $this->assertEquals(count($data), 2);
+
+        $db->deleteAll();
+        $data = $db->readAll();
+        $this->assertEmpty($data);
     }
 
 }
